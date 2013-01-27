474c9596b67c92a06d0d8ed71d8ac8b275246a8f
Nick Sutton <nsutton78-at-gmail-com>
2013-01-27 12:11:15 +0000

    Added the basic structure of the Meeting implementation class

---
+++ src/MeetingImpl.java
@@ -0,0 +1,24 @@
+/**
+ * User: Nick
+ * Date: 27/01/13
+ * Time: 12:08
+ */
+
+import java.util.Calendar;
+import java.util.Set;
+
+public class MeetingImpl implements Meeting {
+
+    public int getId() {
+
+    }
+
+    public Calendar getDate() {
+
+    }
+
+    public Set<Contact> getContacts() {
+
+    }
+
+}
