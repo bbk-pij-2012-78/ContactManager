1d87a46134d7e43d42eb80deb732b9c4919f2b56
Nick Sutton <nsutton78-at-gmail-com>
2013-01-27 15:41:06 +0000

    Updated the ContactImpl to have local variables and return correct fields

--- src/ContactImpl.java
+++ src/ContactImpl.java
@@ -6,19 +6,37 @@
 
 public class ContactImpl implements Contact {
 
-    public int getId() {
+    private int id;
+    private String name;
+    private String notes;
 
+    public ContactImpl(int id, String name, String notes) {
+        this.id = id;
+        this.name = name;
+        this.notes = notes;
+    }
+
+    public int getId() {
+        return this.id;
     }
 
     public String getName() {
-
+        return this.name;
     }
 
     public String getNotes() {
-
+        return this.notes;
     }
 
     public void addNotes(String note) {
+        //if there is already some notes stored then add a line break
+        //and then append the new note
+        if (this.notes.length() > 0) {
+            this.notes = this.notes + "\n" + note;
+        } else {
+            //otherwise just set the note
+            this.notes = note;
+        }
 
     }
 }
