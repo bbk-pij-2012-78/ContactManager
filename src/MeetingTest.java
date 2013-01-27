20b2b21a3a6cc7e706663a5526071d62b73d0f1e
Nick Sutton <nsutton78-at-gmail-com>
2013-01-27 17:23:42 +0000

    Created the basic structure of the MeetingTest class

---
+++ src/MeetingTest.java
@@ -0,0 +1,37 @@
+/**
+ * User: Nick
+ * Date: 27/01/13
+ * Time: 17:20
+ */
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MeetingTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetId() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDate() throws Exception {
+
+    }
+
+    @Test
+    public void testGetContacts() throws Exception {
+
+    }
+}
