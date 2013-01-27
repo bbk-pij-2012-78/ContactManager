dde988f6fd7e0ba594c01de1570bdb94c763cb10
Nick Sutton <nsutton78-at-gmail-com>
2013-01-27 12:07:27 +0000

    Added the basic structure of the Contact implementation class

---
+++ src/ContactImpl.java
@@ -0,0 +1,24 @@
+/**
+ * User: Nick
+ * Date: 27/01/13
+ * Time: 12:03
+ */
+
+public class ContactImpl implements Contact {
+
+    public int getId() {
+
+    }
+
+    public String getName() {
+
+    }
+
+    public String getNotes() {
+
+    }
+
+    public void addNotes(String note) {
+
+    }
+}
