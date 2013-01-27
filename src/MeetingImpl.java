fd87be84ad6c5fd45a8a661d271ed5a0c320524f
Nick Sutton <nsutton78-at-gmail-com>
2013-01-27 16:11:55 +0000

    Updated MeetingImpl with private variables and completed get procedures

--- src/MeetingImpl.java
+++ src/MeetingImpl.java
@@ -9,16 +9,26 @@ import java.util.Set;
 
 public class MeetingImpl implements Meeting {
 
-    public int getId() {
+    private int id;
+    private Calendar meetingDate;
+    private Set<Contact> contacts;
 
+    public MeetingImpl(int id, Calendar meetingDate, Set<Contact> contacts) {
+        this.id = id;
+        this.meetingDate = meetingDate;
+        this.contacts = contacts;
+    }
+
+    public int getId() {
+        return this.id;
     }
 
     public Calendar getDate() {
-
+        return this.meetingDate;
     }
 
     public Set<Contact> getContacts() {
-
+        return this.contacts;
     }
 
 }
