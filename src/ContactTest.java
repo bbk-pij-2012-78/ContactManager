2933d40ba89189ed25c15fbbb86c175c698789eb
Nick Sutton <nsutton78-at-gmail-com>
2013-01-27 15:53:44 +0000

    Implemented the tests for ContactTest

--- src/ContactTest.java
+++ src/ContactTest.java
@@ -9,35 +9,36 @@ import static org.junit.Assert.*;
 
 public class ContactTest {
 
-    private Contact contact;
+    private ContactImpl contact;
 
     @Before
     public void setUp() throws Exception {
-
+        contact = new ContactImpl(1, "John Smith", "these are some notes");
     }
 
     @After
     public void tearDown() throws Exception {
-
+        contact = null;
     }
 
     @Test
     public void testGetId() throws Exception {
-
+        assertEquals(1, contact.getId());
     }
 
     @Test
     public void testGetName() throws Exception {
-
+        assertEquals("John Smith", contact.getName());
     }
 
     @Test
     public void testGetNotes() throws Exception {
-
+        assertEquals("these are some notes", contact.getNotes());
     }
 
     @Test
     public void testAddNotes() throws Exception {
-
+        contact.addNotes("some more notes");
+        assertEquals("these are some notes" + "\n" + "some more notes", contact.getNotes());
     }
 }
