69ba4e08e44fa0ea0b078e1070f8d86c45a426a2
Nick Sutton <nsutton78-at-gmail-com>
2013-01-27 22:11:37 +0000

    Updated the MeetingTest class with first tests

--- src/MeetingTest.java
+++ src/MeetingTest.java
@@ -4,34 +4,40 @@
  * Time: 17:20
  */
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
+import java.util.Calendar;
+import static org.junit.Assert.*;
+import java.util.Set;
 
 public class MeetingTest {
 
+    private MeetingImpl meeting;
+
     @Before
     public void setUp() throws Exception {
-
+        Calendar date = Calendar.getInstance();
+        date.set(2012, 1, 1);
+        Set<Contact> contacts = new Set;
+        meeting = new MeetingImpl(1, date, contacts);
     }
 
     @After
     public void tearDown() throws Exception {
-
+        meeting = null;
     }
 
     @Test
     public void testGetId() throws Exception {
-
+        assertEquals(1, meeting.getId());
     }
 
     @Test
     public void testGetDate() throws Exception {
-
+        fail("Not implemented yet.");
     }
 
     @Test
     public void testGetContacts() throws Exception {
-
+        fail("Not implemented yet.");
     }
 }
