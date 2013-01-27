3e8005f5bee018aa0203b4a4bbbe65c86a02d76f
Nick Sutton <nsutton78-at-gmail-com>
2013-01-27 15:43:33 +0000

    Created the basic structure for the ContactTest class

---
+++ src/ContactTest.java
@@ -0,0 +1,43 @@
+/**
+ * User: Nick
+ * Date: 27/01/13
+ * Time: 14:28
+ */
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ContactTest {
+
+    private Contact contact;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetId() throws Exception {
+
+    }
+
+    @Test
+    public void testGetName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetNotes() throws Exception {
+
+    }
+
+    @Test
+    public void testAddNotes() throws Exception {
+
+    }
+}
