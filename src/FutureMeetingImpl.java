8d9285ef98361de9561415fa521deabb06f974f3
Nick Sutton <nsutton78-at-gmail-com>
2013-01-28 19:13:21 +0000

    Added the contructor to call the super class

--- src/FutureMeetingImpl.java
+++ src/FutureMeetingImpl.java
@@ -3,6 +3,14 @@
  * Date: 27/01/13
  * Time: 12:16
  */
-public class FutureMeetingImpl {
+
+import java.util.Calendar;
+import java.util.Set;
+
+public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting {
+
+    public FutureMeetingImpl(int id, Calendar meetingDate, Set<Contact> contacts) {
+        super(id, meetingDate, contacts);
+    }
 
 }
