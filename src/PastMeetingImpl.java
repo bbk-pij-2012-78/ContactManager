d6d969c7ed10f14fe422ecb1805322f600a3e736
Nick Sutton <nsutton78-at-gmail-com>
2013-01-28 19:11:15 +0000

    Added the notes variable and the constructor calling the super class

--- src/PastMeetingImpl.java
+++ src/PastMeetingImpl.java
@@ -1,3 +1,6 @@
+import java.util.Calendar;
+import java.util.Set;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Nick
@@ -5,9 +8,15 @@
  * Time: 12:12
  * To change this template use File | Settings | File Templates.
  */
-public class PastMeetingImpl implements PastMeeting {
+public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
+
+    private String notes;
+
+    public PastMeetingImpl(int id, Calendar meetingDate, Set<Contact> contacts) {
+        super(id, meetingDate, contacts);
+    }
 
     public String getNotes() {
-
+        return this.notes;
     }
 }
