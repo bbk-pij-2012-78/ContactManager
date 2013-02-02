0674cd4dbe1d94c6dfb53067f3005fe930942c01
Nick Sutton <nsutton78-at-gmail-com>
2013-02-02 20:15:58 +0000

    Added the Test class for ContactManager

---
+++ src/ContactManagerTest.java
@@ -0,0 +1,67 @@
+/**
+ * User: Nick
+ * Date: 02/02/13
+ * Time: 20:15
+ */
+public class ContactManagerTest {
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testAddFutureMeeting() throws Exception {
+
+    }
+
+    public void testGetPastMeeting() throws Exception {
+
+    }
+
+    public void testGetFutureMeeting() throws Exception {
+
+    }
+
+    public void testGetMeeting() throws Exception {
+
+    }
+
+    public void testGetFutureMeetingList() throws Exception {
+
+    }
+
+    public void testGetFutureMeetingList() throws Exception {
+
+    }
+
+    public void testGetPastMeetingList() throws Exception {
+
+    }
+
+    public void testAddNewPastMeeting() throws Exception {
+
+    }
+
+    public void testAddMeetingNotes() throws Exception {
+
+    }
+
+    public void testAddNewContact() throws Exception {
+
+    }
+
+    public void testGetContacts() throws Exception {
+
+    }
+
+    public void testGetContacts() throws Exception {
+
+    }
+
+    public void testFlush() throws Exception {
+
+    }
+}
