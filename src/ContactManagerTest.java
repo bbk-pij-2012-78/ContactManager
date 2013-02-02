eaf869f6142d5588dcb5127e7493e8f07f9380c9
Nick Sutton <nsutton78-at-gmail-com>
2013-02-02 20:31:16 +0000

    Added a test for addFutureMeeting to check a valid meeting is added

--- src/ContactManagerTest.java
+++ src/ContactManagerTest.java
@@ -3,65 +3,98 @@
  * Date: 02/02/13
  * Time: 20:15
  */
+
+import org.junit.*;
+
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
 public class ContactManagerTest {
+
+    private ContactManager contactManager;
+
+    @Before
     public void setUp() throws Exception {
-        super.setUp();
-
+        contactManager = new ContactManagerImpl();
     }
 
+    @After
     public void tearDown() throws Exception {
-
+        contactManager = null;
     }
 
+    @Test
     public void testAddFutureMeeting() throws Exception {
+        Set<Contact> contacts = new HashSet<>();
+        contacts.add(new ContactImpl(1, "John Smith", "some notes"));
 
+        Calendar date = Calendar.getInstance();
+        date.set(25, 2, 2013);
+
+        assertEquals(0, contactManager.addFutureMeeting(contacts, date));
     }
 
+    /*
+    @Test
     public void testGetPastMeeting() throws Exception {
 
     }
 
+    @Test
     public void testGetFutureMeeting() throws Exception {
 
     }
 
+    @Test
     public void testGetMeeting() throws Exception {
 
     }
 
+    @Test
     public void testGetFutureMeetingList() throws Exception {
 
     }
 
+    @Test
     public void testGetFutureMeetingList() throws Exception {
 
     }
 
+    @Test
     public void testGetPastMeetingList() throws Exception {
 
     }
 
+    @Test
     public void testAddNewPastMeeting() throws Exception {
 
     }
 
+    @Test
     public void testAddMeetingNotes() throws Exception {
 
     }
 
+    @Test
     public void testAddNewContact() throws Exception {
 
     }
 
+    @Test
     public void testGetContacts() throws Exception {
 
     }
 
+    @Test
     public void testGetContacts() throws Exception {
 
     }
 
+    @Test
     public void testFlush() throws Exception {
 
-    }
+    } */
 }
