89cb9a0d2f6309f3ce25631e45b4b58c1b9b17fe
Nick Sutton <nsutton78-at-gmail-com>
2013-02-04 17:05:19 +0000

    Added the tests for GetMeeting and GetFutureMeeting

--- src/ContactManagerTest.java
+++ src/ContactManagerTest.java
@@ -60,18 +60,56 @@ public class ContactManagerTest {
     @Test
     public void testGetPastMeeting() throws Exception {
 
-    }
+    } */
 
     @Test
     public void testGetFutureMeeting() throws Exception {
+        Calendar fc = Calendar.getInstance();
 
+        //create a list of contacts to add
+        Set<Contact> contacts = new HashSet<>();
+        contacts.add(new ContactImpl(1, "John Smith", "some notes" ));
+        contacts.add(new ContactImpl(2, "Peter Smith", "some other notes" ));
+        contacts.add(new ContactImpl(3, "Alan Smith", "notes" ));
+
+        //add two new future meetings
+        fc.add(Calendar.DATE, 1);  //ensure the date is in the future
+        int m1 = contactManager.addFutureMeeting(contacts, fc);
+        int m2 = contactManager.addFutureMeeting(contacts, fc);
+
+        //execute the getMeeting procedure and compare the IDs
+        assertEquals(m1, contactManager.getFutureMeeting(m1).getId());
+        assertEquals(m2, contactManager.getFutureMeeting(m2).getId());
+
+        //test that a null object is sent back for a meeting that doesn't exist
+        assertEquals(null, contactManager.getFutureMeeting(99));
     }
 
     @Test
     public void testGetMeeting() throws Exception {
+        Calendar fc = Calendar.getInstance();
 
+        //create a list of contacts to add
+        Set<Contact> contacts = new HashSet<>();
+        contacts.add(new ContactImpl(1, "John Smith", "some notes" ));
+        contacts.add(new ContactImpl(2, "Peter Smith", "some other notes" ));
+        contacts.add(new ContactImpl(3, "Alan Smith", "notes" ));
+
+        //add two new future meetings
+        fc.add(Calendar.DATE, 1);  //ensure the date is in the future
+        int m1 = contactManager.addFutureMeeting(contacts, fc);
+        int m2 = contactManager.addFutureMeeting(contacts, fc);
+
+        //add two new past meetings
+        fc.add(Calendar.DATE, -1);  //ensure the date is in the future
+        contactManager.addNewPastMeeting(contacts, fc, "meeting 3 notes");
+        contactManager.addNewPastMeeting(contacts, fc, "meeting 4 notes");
+
+        //execute the getMeeting procedure and compare the IDs
+        assertEquals(m1, contactManager.getMeeting(m1).getId());
+        assertEquals(m2, contactManager.getMeeting(m2).getId());
     }
-
+     /*
     @Test
     public void testGetFutureMeetingList() throws Exception {
 
