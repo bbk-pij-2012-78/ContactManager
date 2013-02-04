c7565c257c2778c3dbac8d6c3da5918b050aef59
Nick Sutton <nsutton78-at-gmail-com>
2013-02-04 14:42:03 +0000

    Added a Test class for the PastMeetingImpl class to test the getNotes procedure

---
+++ src/PastMeetingImplTest.java
@@ -0,0 +1,27 @@
+/**
+ * User: Nick
+ * Date: 04/02/13
+ * Time: 14:28
+ */
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
+
+public class PastMeetingImplTest {
+
+    @Test
+    public void testGetNotes() throws Exception {
+        Set<Contact> contacts = new HashSet<>();
+        Calendar date = Calendar.getInstance();
+        String notes = "Some notes";
+        PastMeeting pm = new PastMeetingImpl(1, date, contacts, notes);
+
+        //test the ID so that we know the super constructor is working
+        assertEquals(pm.getId(), 1);
+        //now test that get notes returns the correct string
+        assertEquals(pm.getNotes(), notes);
+    }
+}
