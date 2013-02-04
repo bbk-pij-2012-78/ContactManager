18be0c528f6ad561b12b5d6dd37c7f0bf1034559
Nick Sutton <nsutton78-at-gmail-com>
2013-02-04 15:32:42 +0000

    Renamed the PastMeetingTest file as it was incorrectly named and replaced it with the correctly named file

--- src/PastMeetingTest.java
+++ src/PastMeetingTest.java
@@ -10,7 +10,7 @@ import java.util.Calendar;
 import java.util.HashSet;
 import java.util.Set;
 
-public class PastMeetingImplTest {
+public class PastMeetingTest {
 
     @Test
     public void testGetNotes() throws Exception {
